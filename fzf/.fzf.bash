234f5bb80fe3b251af60c79f9ae5d0dafc962eda
Nikhil Singh <nik.singh710@gmail.com>
2022-01-24 20:37:46 +0530

    fzf home path updated to be relative

--- fzf/.fzf.bash
+++ fzf/.fzf.bash
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/niksingh710/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/home/niksingh710/.fzf/bin"
+if [[ ! "$PATH" == *$HOME/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}$HOME/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/niksingh710/.fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "$HOME/.fzf/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/niksingh710/.fzf/shell/key-bindings.bash"
+source "$HOME/.fzf/shell/key-bindings.bash"
